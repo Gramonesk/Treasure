22fd479e1328aea6bf108399a226841b22c67bb3
ORIAMM <riovalgm.13@gmail.com>
2024-06-17 02:21:33 +0700

    Update Panel

--- Software Engineering/Assets/Script2/Panel Player Handler.cs
+++ Software Engineering/Assets/Script2/Panel Player Handler.cs
@@ -7,6 +7,8 @@ public class PanelPlayerHandler : MonoBehaviour
     [Header("MainMenu Scene")]
     public Transform panelPlayer;
     public GameObject PanelPrefab;
+    public bool _isReady = false;
+    public string status = "Ready";
 
     public void Start()
     {
@@ -29,4 +31,26 @@ public class PanelPlayerHandler : MonoBehaviour
         /*GameObject newEntry = GameObject.Destroy(PanelPlayerPrefab);*/
 
     }
+
+    public void UpdatePlayerStatus()
+    {
+        Debug.Log("Update Ready");
+        PanelPlayerPrefab readyEntry = PanelPrefab.GetComponent<PanelPlayerPrefab>();
+        if (_isReady ==  false)
+        {
+            Debug.Log("READYYYYYYYYYYY SIRRRRRRRRR");
+            readyEntry.ReadyStatus.text = status.ToString();
+            Debug.Log($"Status = {readyEntry.ReadyStatus.text}");
+            readyEntry.ReadyStatus.color = Color.green;
+            _isReady = true;
+        }
+        else
+        {
+            readyEntry.ReadyStatus.text = "Not Ready";
+            readyEntry.ReadyStatus.color = Color.red;
+            _isReady = false;
+        }
+    }
+
+
 }
